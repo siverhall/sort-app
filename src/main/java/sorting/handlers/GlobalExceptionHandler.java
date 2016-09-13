77a0e71769f6be33a3eca4ed80c7067ef05884f6
siverhalle <emil.siverhall@cgi.com>
2016-09-13 15:17:37 +0200

    added exception handler for empty request body

--- src/main/java/sorting/handlers/GlobalExceptionHandler.java
+++ src/main/java/sorting/handlers/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package sorting.handlers;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -10,14 +11,20 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NumberFormatException.class)
-    @ResponseStatus(value = HttpStatus.PRECONDITION_FAILED, reason = "Invalid number array")
-    public void handleInvalidInput(Exception e) {
+    @ResponseStatus(value = HttpStatus.PRECONDITION_FAILED, reason = "Invalid number array.")
+    public void handleInvalidInput() {
         // Respond with a HttpStatus of 412 - Precondition failed
     }
 
     @ExceptionHandler(DataIntegrityViolationException.class)
-    @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED, reason = "Unable to store result")
-    public void handleDatabaseErrors(Exception e) {
+    @ResponseStatus(value = HttpStatus.EXPECTATION_FAILED, reason = "Unable to store result.")
+    public void handleDatabaseErrors() {
         // Respond with a HttpStatus of 417 - Expectation failed
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Request body is missing.")
+    public void handleEmptyPosts() {
+        // Respond with a HttpStatus of 400 - Bad request
     }
 }
